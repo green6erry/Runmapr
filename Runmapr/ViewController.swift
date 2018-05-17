01ae537849b9bccb2757d9548cbdaa1f158cc381
green6erry <green6erry@gmail.com>
2018-05-17 13:22:37 -0400

    Initial commit

---
+++ Runmapr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Runmapr
+//
+//  Created by Amanda Brown on 5/17/18.
+//  Copyright © 2018 AGB Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
