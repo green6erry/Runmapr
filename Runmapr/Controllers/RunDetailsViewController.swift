11aad4c18ae231e92bb033ba8e7f10fd7b0c5f70
green6erry <green6erry@gmail.com>
2018-05-29 09:02:54 -0400

    polylines not working

--- Runmapr/Controllers/RunDetailsViewController.swift
+++ Runmapr/Controllers/RunDetailsViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 import Firebase
 
-class RunDetailsViewController: UIViewController {
+class RunDetailsViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var durationLabel: UILabel!
@@ -21,21 +21,28 @@ class RunDetailsViewController: UIViewController {
     var coordinates = [CLLocationCoordinate2D]()
     var latitudes = [Double]()
     var longitudes = [Double]()
+    var polylines = [MKPolyline]()
+    
+    let sampleLocations = [
+            CLLocationCoordinate2D(latitude: 32.7767, longitude: -96.798),
+            CLLocationCoordinate2D(latitude: 37.7833, longitude: -122.4167),
+            CLLocationCoordinate2D(latitude: 42.2814, longitude: -83.7483)
+            ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.selectedTrip.date = (dateLabel.text as String?)!
+        mapView.delegate = self
+        
         durationLabel.text = "Duration: \(selectedTrip.duration)"
         distanceLabel.text = "Distance: \(selectedTrip.distance)"
         dateLabel.text = "Date: \(selectedTrip.date)"
         
         DatabaseService.shared.coordsReference.child("\(selectedTrip.tripId)").observe(DataEventType.value) { (snapshot) in
-            print(snapshot)
+//            print("1snapshot \(snapshot)")
             guard let coordsSnapshot = CoordSnap(with: snapshot) else { return }
             self.coords = coordsSnapshot.coords
-//            self.coordinates = self.coords.
-            print("coords count \(self.coords.count)")
-            print("coords smaple \(self.coords[1].longitude)")
+//            print("2coords count \(self.coords.count)")
+//            print("3coords smaple \(self.coords[1].longitude)")
             
             for index in 1...self.coords.count - 1 {
                 let longitude = Double(self.coords[index].longitude)
@@ -43,40 +50,59 @@ class RunDetailsViewController: UIViewController {
                 self.longitudes.append(longitude!)
                 self.latitudes.append(latitude!)
                 let newCoordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(latitude!), longitude: CLLocationDegrees(longitude!))
-                self.coordinates.append(newCoordinate)
+                self.coordinates.insert(newCoordinate, at: 0)
+                if self.coordinates.count > 2{
+                    let coordTuple : [CLLocationCoordinate2D]
+                    coordTuple = [self.coordinates[0], self.coordinates[1]]
+                    self.mapView.add(MKPolyline(coordinates: coordTuple, count: 2))
+                }
             }
-            print("coordinates \(self.coordinates)")
+            
+            //coordinates have been added
+//            print("4coordinates \(self.coordinates)")
+//            print("5longitudes \(self.longitudes)")
+//            print("6latitudes \(self.latitudes)")
 
             
             let region = self.mapRegion()
             self.mapView.setRegion(region!, animated: true)
             self.mapView.add(MKPolyline(coordinates: self.coordinates, count: self.coordinates.count))
-            
-            
+//            self.mapView.add(MKPolyline(coordinates: self.coordinates, count: 4))
         }
-
     }
+    
+    
     private func mapRegion() -> MKCoordinateRegion? {
+        
         let maxLat = self.latitudes.max()!
         let maxLong = self.longitudes.max()!
         let minLat = self.latitudes.min()!
         let minLong = self.longitudes.min()!
         
+        let params: [String: Double] = ["maxLat" : self.latitudes.max()!,
+                      "maxLong" : self.longitudes.max()!,
+                      "minLat" : self.latitudes.min()!,
+                      "minLong" : self.longitudes.min()!]
+        
+        print("8params \(params)")
+        
         let center = CLLocationCoordinate2D(latitude: (minLat + maxLat) / 2,
                                             longitude: (minLong + maxLong) / 2)
         let span = MKCoordinateSpan(latitudeDelta: (maxLat - minLat) * 1.3,
                                     longitudeDelta: (maxLong - minLong) * 1.3)
+
         return MKCoordinateRegion(center: center, span: span)
     }
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
         let polylineRender = MKPolylineRenderer(overlay: overlay)
         polylineRender.strokeColor = UIColor.red.withAlphaComponent(0.5)
         polylineRender.lineWidth = 5
         
         return polylineRender
     }
-    
+
     
         
 }
